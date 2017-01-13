bcff3d8c33eb4eb1516854ed095c8ec7e57b3e2c
Double Helix <doublesine@tju.edu.cn>
2017-01-13 22:32:41 +0800

    Update CRC.h and CRC.cpp

--- DataVerification/CRC.cpp
+++ DataVerification/CRC.cpp
@@ -3,13 +3,13 @@
 BYTE CRC8::CRC8Table[] = {0};
 void CRC8::GenerateTable(BYTE Polynomial) {
     for(UINT16 i = 0; i < 0x100; i++) {
-        BYTE A = (BYTE)i;
+        BYTE Present_i = (BYTE)i;
         BYTE Result = 0;
         for(BYTE j = 1, pointer; j <= 8; j++) {
             pointer = 0x80 >> (j - 1);
-            if(pointer & A) {
-                A ^= pointer;
-                A ^= (Polynomial >> j);
+            if(pointer & Present_i) {
+                Present_i ^= pointer;
+                Present_i ^= (Polynomial >> j);
                 Result ^= Polynomial << (8 - j);
             }
         }
@@ -19,14 +19,68 @@ void CRC8::GenerateTable(BYTE Polynomial) {
 }
 
 BYTE CRC8::Calculate(const BYTE* srcData, UINT64 srcDataLength, BYTE GenerationPolynomial) {
-    BYTE Result = srcData[0];
+    if(srcDataLength == 0 || srcData == NULL) return 0;
+    BYTE Result = 0;
     if(GenerationPolynomial != CRC8Table[256]) GenerateTable(GenerationPolynomial);
-    for(UINT64 i = 1; i < srcDataLength; i++) {
+    for(UINT64 i = 0; i < srcDataLength; i++) {
         Result = CRC8Table[Result] ^ srcData[i];
     }
     return CRC8Table[Result];
 }
 
-BOOL CRC8::CheckData(const BYTE* srcData, UINT64 srcDataLength, BYTE GenerationPolynomial, BYTE CRCValue) {
-    return CRCValue == Calculate(srcData, srcDataLength, GenerationPolynomial) ? TRUE : FALSE;
+BOOL CRC8::CheckData(const BYTE* srcData, UINT64 srcDataLength, BYTE GenerationPolynomial, BYTE CRC8Value) {
+    return CRC8Value == Calculate(srcData, srcDataLength, GenerationPolynomial) ? TRUE : FALSE;
 }
+
+UINT16 CRC16::CRC16Table[257] = {0};
+void CRC16::GenerateTable(UINT16 Polynomial) {
+    BYTE Polynomial_H = *(BYTE*)&Polynomial;
+    BYTE Polynomial_L = *(BYTE*)(&Polynomial + 1);
+    for(UINT16 i = 0; i < 0x100; i++) {
+        BYTE Present_i = (BYTE)i;
+        UINT16 Result = 0;
+        for(BYTE j = 1, pointer; j <= 8; j++) {
+            pointer = 0x80 >> (j - 1);
+            if(pointer & Present_i) {
+                Present_i ^= pointer;
+                Present_i ^= (Polynomial_H >> j);
+                *(BYTE*)&Result ^= ((Polynomial_H << (8 - j)) + (Polynomial_L >> j));
+                *(BYTE*)(&Result + 1) ^= Polynomial_L << (8 - j);
+            }
+        }
+        CRC16Table[i] = Result;
+    }
+    CRC16Table[256] = Polynomial;
+}
+
+UINT16 CRC16::Calculate(const BYTE* srcData, UINT64 srcDataLength, UINT16 GenerationPolynomial) {
+    if(srcDataLength == 0 || srcData == NULL) return 0;
+    UINT16 Result = 0;
+    if(GenerationPolynomial != CRC16Table[256]) GenerateTable(GenerationPolynomial);
+    for(UINT64 i = 0; i < srcDataLength; i++) {
+        *(*BYTE)&Result = CRC16Table[*(*BYTE)&Result] ^ srcData[i];
+        *(*BYTE)(&Result + 1) = CRC16Table[*(*BYTE)(&Result + 1)] ^ srcData[i + 1];
+    }
+
+}
+
+BOOL CRC16::CheckData(const BYTE* srcData, UINT64 srcDataLength, UINT16 GenerationPolynomial, UINT16 CRC16Value) {
+    return CRC16Value == Calculate(srcData, srcDataLength, GenerationPolynomial) ? TRUE : FALSE;
+}
+
+BYTE CRC32::CRC32Table_HH[257] = {0};
+BYTE CRC32::CRC32Table_HL[257] = {0};
+BYTE CRC32::CRC32Table_LH[257] = {0};
+BYTE CRC32::CRC32Table_LL[257] = {0};
+void CRC32::GenerateTable(UINT32 Polynomial) {
+
+}
+
+UINT32 CRC32::Calculate(const BYTE* srcData, UINT64 srcDataLength, UINT32 GenerationPolynomial) {
+
+}
+
+BOOL CRC32::CheckData(const BYTE* srcData, UINT64 srcDataLength, UINT32 GenerationPolynomial, UINT32 CRC32Value) {
+    return CRC32Value == Calculate(srcData, srcDataLength, GenerationPolynomial) ? TRUE : FALSE;
+}
+
--- DataVerification/CRC.h
+++ DataVerification/CRC.h
@@ -13,5 +13,33 @@ public:
     static BOOL CheckData(const BYTE* srcData, UINT64 srcDataLength, BYTE GenerationPolynomial, BYTE CRC8Value);
 };
 
+class CRC16 {
+private:
+    static UINT16 CRC16Table[257];
+
+    CRC16();
+    static void GenerateTable(UINT16 Polynomial);
+public:
+    /**Here, GenerationPolynomial should use Big-Endian, which means that you should input 0x5623 if you want to set GenerationPolynomial 0x2356 where 0x23 is high 8-bits and 0x56 is low 8-bits.*/
+    static UINT16 Calculate(const BYTE* srcData, UINT64 srcDataLength, UINT16 GenerationPolynomial);
+    /**Here, GenerationPolynomial should use Big-Endian, which means that you should input 0x5623 if you want to set GenerationPolynomial 0x2356 where 0x23 is high 8-bits and 0x56 is low 8-bits. So does CRC16Value.*/
+    static BOOL CheckData(const BYTE* srcData, UINT64 srcDataLength, UINT16 GenerationPolynomial, UINT16 CRC16Value);
+};
+
+class CRC32 {
+private:
+    static BYTE CRC32Table_HH[257];
+    static BYTE CRC32Table_HL[257];
+    static BYTE CRC32Table_LH[257];
+    static BYTE CRC32Table_LL[257];
+
+    CRC32();
+    static void GenerateTable(UINT32 Polynomial);
+public:
+    /**Here, GenerationPolynomial should use Big-Endian, which means that you should input 0x78563412 if you want to set GenerationPolynomial 0x12345678 where 0x12 is the highest 8-bits and 0x78 is lowest 8-bits.*/
+    static UINT32 Calculate(const BYTE* srcData, UINT64 srcDataLength, UINT32 GenerationPolynomial);
+    /**Here, GenerationPolynomial should use Big-Endian, which means that you should input 0x78563412 if you want to set GenerationPolynomial 0x12345678 where 0x12 is the highest 8-bits and 0x78 is lowest 8-bits. So does CRC32Value.*/
+    static BOOL CheckData(const BYTE* srcData, UINT64 srcDataLength, UINT32 GenerationPolynomial, UINT32 CRC32Value);
+};
 
 #endif // CRC_H_INCLUDED
