48d06ddbf891880b808fbb914e42b4e504fd590c
Double Helix <doublesine@tju.edu.cn>
2017-01-21 23:04:17 +0800

    Optimized the column mix in AES128Encryption::Cipher

--- Encryption/AES128.h
+++ Encryption/AES128.h
@@ -6,6 +6,8 @@ class AES128Encryption {
 private:
     static BYTE SBox[256];
     static UINT32 Rcon[11];
+    static BYTE Multiply0x02[256];
+    static BYTE Multiply0x03[256];
     AES128Encryption();
 public:
     static void Cipher(BYTE srcBytes[16], UINT32 srcExpandedKey[44]);
