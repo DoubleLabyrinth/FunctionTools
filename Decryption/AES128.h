1f72d0141b61c3abb0386cae2ca0d8defe6983f2
Double Helix <doublesine@tju.edu.cn>
2017-01-21 23:35:26 +0800

    Added AES128.h and AES128.cpp
    Added AES128 decryption.

---
+++ Decryption/AES128.h
@@ -0,0 +1,20 @@
+#ifndef AES128_DECRYPTION_H_INCLUDED
+#define AES128_DECRYPTION_H_INCLUDED
+#include "../TypeDefine.h"
+
+class AES128Decryption {
+private:
+    static BYTE SBox[256];
+    static BYTE InverseSBox[256];
+    static UINT32 Rcon[11];
+    static BYTE Multiply0x09[256];
+    static BYTE Multiply0x0B[256];
+    static BYTE Multiply0x0D[256];
+    static BYTE Multiply0x0E[256];
+    AES128Decryption();
+public:
+    static void InverseCipher(BYTE srcBytes[16], UINT32 srcExpandedKey[44]);
+    static void KeyExpansion(BYTE srcKey[16], UINT32 dstExpandedKey[44]);
+};
+
+#endif // AES128_DECRYPTION_H_INCLUDED
