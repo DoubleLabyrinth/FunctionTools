5c2289e15857a9a732a10996dc8bb83e46f077e0
Double Helix <doublesine@tju.edu.cn>
2017-01-23 15:33:05 +0800

    Optimized Base64 algorithm

--- Encoding/Base/Base64.cpp
+++ Encoding/Base/Base64.cpp
@@ -17,39 +17,42 @@ char Base64::URL_Filename_Safe_Base64Table[64] = {'A', 'B', 'C', 'D', 'E', 'F', 
                                                   'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
                                                   'w', 'x', 'y', 'z', '0', '1', '2', '3',
                                                   '4', '5', '6', '7', '8', '9', '-', '_'};
+
 SmartPointer<char> Base64::GetEncodedString(const BYTE* srcBytes, UINT64 BytesLength, bool URL_Filename_Safe) {
     char* TransTable = URL_Filename_Safe ? URL_Filename_Safe_Base64Table : Base64Table;
-    UINT64 TimesOfCycle = BytesLength / 3;
-    UINT64 EncodedStringLength = TimesOfCycle * 4;
-    BYTE LeftoverBytesNumber = BytesLength % 3;
-    EncodedStringLength += LeftoverBytesNumber == 0 ? 0 : 4;
-    char* Ret = (char*)malloc(sizeof(char) * EncodedStringLength + 1);
+    UINT64 TimesOfMainCycle = BytesLength / 3;
+    BYTE LeftoverBytesCount = BytesLength % 3;
+    char* Ret = (char*)malloc(sizeof(char) * (TimesOfMainCycle << 2) + (LeftoverBytesCount == 0 ? 1 : 5));
     if(Ret == nullptr) return nullptr;
 
-    for(UINT64 i = 0; i < TimesOfCycle; i++) {
-        Ret[i * 4] = TransTable[(srcBytes[3 * i] & 0xFC) >> 2];
-        Ret[i * 4 + 1] = TransTable[(srcBytes[3 * i] & 0x03) << 4 | (srcBytes[3 * i + 1] & 0xF0) >> 4];
-        Ret[i * 4 + 2] = TransTable[(srcBytes[3 * i + 1] & 0x0F) << 2 | (srcBytes[3 * i + 2] & 0xC0) >> 6];
-        Ret[i * 4 + 3] = TransTable[srcBytes[3 * i + 2] & 0x3F];
+    UINT64 OffsetOfsrcBytes = 0;
+    UINT64 OffsetOfRet = 0;
+    for(UINT64 i = 0; i < TimesOfMainCycle; i++) {
+        Ret[OffsetOfRet] = TransTable[srcBytes[OffsetOfsrcBytes] >> 2];
+        Ret[OffsetOfRet + 1] = TransTable[(srcBytes[OffsetOfsrcBytes] & 0x03) << 4 | srcBytes[OffsetOfsrcBytes + 1] >> 4];
+        Ret[OffsetOfRet + 2] = TransTable[(srcBytes[OffsetOfsrcBytes + 1] & 0x0F) << 2 | srcBytes[OffsetOfsrcBytes + 2] >> 6];
+        Ret[OffsetOfRet + 3] = TransTable[srcBytes[OffsetOfsrcBytes + 2] & 0x3F];
+        OffsetOfRet += 4;
+        OffsetOfsrcBytes += 3;
     }
 
-    switch(LeftoverBytesNumber) {
+    switch(LeftoverBytesCount) {
         case 0:
-            Ret[TimesOfCycle * 4] = '\0';
+            Ret[OffsetOfRet] = '\0';
             return Ret;
         case 1:
-            Ret[TimesOfCycle * 4] = TransTable[(srcBytes[BytesLength - 1] & 0xFC) >> 2];
-            Ret[TimesOfCycle * 4 + 1] = TransTable[(srcBytes[BytesLength - 1] & 0x03) << 4];
-            Ret[TimesOfCycle * 4 + 2] = '=';
-            Ret[TimesOfCycle * 4 + 3] = '=';
-            Ret[TimesOfCycle * 4 + 4] = '\0';
+            Ret[OffsetOfRet] = TransTable[srcBytes[OffsetOfsrcBytes] >> 2];
+            Ret[OffsetOfRet + 1] = TransTable[(srcBytes[OffsetOfsrcBytes] & 0x03) << 4];
+            Ret[OffsetOfRet + 2] = '=';
+            Ret[OffsetOfRet + 3] = '=';
+            Ret[OffsetOfRet + 4] = '\0';
             return Ret;
         case 2:
-            Ret[TimesOfCycle * 4] = TransTable[(srcBytes[BytesLength - 2] & 0xFC) >> 2];
-            Ret[TimesOfCycle * 4 + 1] = TransTable[(srcBytes[BytesLength - 2] & 0x03) << 4 | (srcBytes[BytesLength - 1] & 0xF0) >> 4];
-            Ret[TimesOfCycle * 4 + 2] = TransTable[(srcBytes[BytesLength - 1] & 0xF) << 2];
-            Ret[TimesOfCycle * 4 + 3] = '=';
-            Ret[TimesOfCycle * 4 + 4] = '\0';
+            Ret[OffsetOfRet] = TransTable[srcBytes[OffsetOfsrcBytes] >> 2];
+            Ret[OffsetOfRet + 1] = TransTable[(srcBytes[OffsetOfsrcBytes] & 0x03) << 4 | srcBytes[OffsetOfsrcBytes + 1] >> 4];
+            Ret[OffsetOfRet + 2] = TransTable[(srcBytes[OffsetOfsrcBytes + 1] & 0x0F) << 2];
+            Ret[OffsetOfRet + 3] = '=';
+            Ret[OffsetOfRet + 4] = '\0';
             return Ret;
         default:
             free(Ret);
